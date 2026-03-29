7b8138ac704df2e8edccf1e79a5498376137b727
shaoli-lu <shaoli_lu@yahoo.com>
2026-03-29 08:51:55 -0500

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body>
         {children}
